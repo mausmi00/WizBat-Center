78d17fcbb9b2ea57d3685f6b667413a036632109
mausmi00 <mausmi000@gmail.com>
2023-07-15 03:55:45 +0530

    store: impl product page

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,7 +1,26 @@
-const HomePage = () => {
-    return ( <div>
-       Home Store
-    </div> );
-}
- 
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/productList";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3d6d96de-cb4b-4595-9114-04601edf6bb8");
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
