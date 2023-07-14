49671153eeaf3c48b9ee335fd01e339f46a7c305
mausmi00 <mausmi000@gmail.com>
2023-07-14 20:03:02 +0530

    added store navbar

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
     return ( <div>
-        home page
+       Home Store
     </div> );
 }
  
--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -1,8 +1,25 @@
-const Navbar = () => {
-    return ( <div className="border-b">
-        NavBar
+import Container from "@/components/ui/container";
+import Link from "next/link";
+import MainNav from "./main-nav";
+import getCategories from "@/actions/getCategories";
+import NavbarActions from "@/components/navbar-actions";
 
-    </div> );
-}
- 
+const Navbar = async () => {
+  const categories = await getCategories();
+
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Navbar;
