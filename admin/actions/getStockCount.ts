bed55828a3d8fe5642a70b1098a56f6ee3eac27c
mausmi00 <mausmi000@gmail.com>
2023-07-17 03:14:41 +0530

    admin: impl overview page

---
+++ admin/actions/getStockCount.ts
@@ -0,0 +1,13 @@
+import prismadb from "@/lib/prismadb";
+
+const getStockCount = async (storeId: string) => {
+    const stockCount = await prismadb.product.count({
+        where: {
+            storeId,
+            isArchived: false,
+        },
+    });
+    return stockCount
+}
+
+export default getStockCount;
