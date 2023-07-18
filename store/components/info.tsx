73dd3c5363c5ccb4779ddcab87a730012de806af
mausmi00 <mausmi000@gmail.com>
2023-07-18 23:11:28 +0530

    store: fixed add to cart issue

--- store/components/info.tsx
+++ store/components/info.tsx
@@ -4,12 +4,17 @@ import { Product } from "@/types";
 import Currency from "./ui/currency";
 import { ShoppingCart } from "lucide-react";
 import Button from "./ui/button";
+import useCart from "@/hooks/useCart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -35,7 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Card
           <ShoppingCart />
         </Button>
