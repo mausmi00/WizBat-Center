fbe5f2651df88d5764d583a02f26f7f1132ebee7
mausmi00 <mausmi000@gmail.com>
2023-07-26 02:01:37 +0530

    fixed socket issue/updated agent ui

--- admin/app/(dashboard)/[storeId]/(routes)/robin/components/sheetDisplay.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/robin/components/sheetDisplay.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Pointer } from "lucide-react";
+import { Pointer, X } from "lucide-react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { AiOutlineQq, AiOutlineSend } from "react-icons/ai";
 import {
@@ -11,6 +11,7 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
+  SheetClose,
 } from "@/components/ui/sheet";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -25,6 +26,7 @@ import { toast } from "react-hot-toast";
 import { Conversation, Message } from "@prisma/client";
 import Form from "./form";
 import { auth, useUser } from "@clerk/nextjs";
+import clsx from "clsx";
 
 declare global {
   var socket: any;
@@ -32,8 +34,10 @@ declare global {
 
 const SheetDisplay = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
   const [messages, setMessages] = useState(null);
   const [newMessageSent, setNewMessageSent] = useState(false);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
   //server connection
   const [isConnected, setIsConnected] = useState(false);
@@ -66,8 +70,21 @@ const SheetDisplay = () => {
   //   };
   let convoId = null;
 
-  const onSocketClose = () => {
+  const onClose = () => {
+    setIsLoading(true);
+    axios
+      .delete(`/api/${storeId}/robin`)
+      .then((data) => {
+        console.log("data: ", data);
+        setMessages(null);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    setOpen(false);
+    socket?.current?.close();
     setIsConnected(false);
+    // delete conversation messages
   };
 
   const onConnect = useCallback(() => {
@@ -81,7 +98,7 @@ const SheetDisplay = () => {
           name: globalThis.globalThis.user?.firstName,
         })
       );
-      socket.current?.addEventListener("close", onSocketClose);
+      socket.current?.addEventListener("close", onClose);
       socket.current?.addEventListener("message", (event: { data: string }) => {
         // onSocketMessage(event.data);
         setNewMessageSent(true);
@@ -98,11 +115,13 @@ const SheetDisplay = () => {
         setMessages(data.data);
         convoId = data.data[1];
         setNewMessageSent(false);
+        bottomRef?.current?.scrollIntoView();
       })
       .catch(() => toast.error("Something went wrong!"));
   }, [newMessageSent]);
 
   const onClick = () => {
+    setOpen(true);
     axios
       .get(`/api/${storeId}/robin`)
       .then((data) => {
@@ -117,13 +136,25 @@ const SheetDisplay = () => {
 
   return (
     <>
-      <Sheet>
+      <Sheet open={open}>
         <SheetTrigger>
-          <AiOutlineQq size={25} className="cursor-pointer" onClick={onClick} />
+          {isLoading ? (
+            <AiOutlineQq size={25} className={clsx(`cursor-wait`)} />
+          ) : (
+            <AiOutlineQq
+              size={25}
+              className={clsx(`cursor-pointer`)}
+              onClick={onClick}
+            />
+          )}
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Robin</SheetTitle>
+            <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+              <X className="h-4 w-4" onClick={onClose} />
+              <span className="sr-only">Close</span>
+            </SheetClose>
             <SheetDescription>
               Because every Batman needs a Robin.
             </SheetDescription>
@@ -131,6 +162,7 @@ const SheetDisplay = () => {
           <div className="h-full flex flex-col">
             <div className="flex-1 overflow-y-auto">
               <MessageBody initialMessages={messages} />
+              <div className="pt-15" ref={bottomRef} />
             </div>
             <div
               className="
