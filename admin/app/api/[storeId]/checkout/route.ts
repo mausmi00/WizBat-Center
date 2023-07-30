d7fdb0ed57e8cd5b1c98077dab249f2f381fe881
mausmi00 <mausmi000@gmail.com>
2023-07-31 03:01:48 +0530

    fixed payment issue

--- admin/app/api/[storeId]/checkout/route.ts
+++ admin/app/api/[storeId]/checkout/route.ts
@@ -4,23 +4,26 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
+    'Access-Control-Allow-Origin': '*',
     "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization"
 };
 
 // before a post request we need to make a request with options
 export async function OPTIONS() {
-    return NextResponse.json({}, { headers: corsHeaders });
+    return NextResponse.json({}, { headers: corsHeaders })
 };
 
 export async function POST(req: Request, { params }: { params: { storeId: string } }) {
 
     const { productIds } = await req.json();
-    
+
     if (!productIds || productIds.length === 0) {
         return new NextResponse("Product ids are required", { status: 400 });
     }
+
+    // Set CORS headers
+    const responseHeaders = new Headers(corsHeaders);
 
     const products = await prismadb.product.findMany({
         where: {
@@ -31,13 +34,14 @@ export async function POST(req: Request, { params }: { params: { storeId: string
     });
 
     const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
     products.forEach((product) => {
         line_items.push({
             quantity: 1,
             price_data: {
                 currency: 'USD',
                 product_data: {
-                    name: product.name
+                    name: product.name,
                 },
                 unit_amount: product.price.toNumber() * 100
             }
@@ -73,7 +77,17 @@ export async function POST(req: Request, { params }: { params: { storeId: string
             orderId: order.id
         }
     });
-    return NextResponse.json({ url: session.url }, {
-        headers: corsHeaders
+
+    // Return the response with CORS headers
+    const responseBody = JSON.stringify({ url: session.url });
+    return new Response(responseBody, {
+        status: 200,
+        headers: responseHeaders,
+        statusText: 'OK',
     });
+
+
+    // return NextResponse.json({ url: session.url }, {
+    //     headers: corsHeaders
+    // });
 }
